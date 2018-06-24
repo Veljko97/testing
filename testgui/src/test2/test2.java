57ad318e9c89983141500fa644352567799b4842
vmvuc <vmvuc@192.168.0.184>
2018-06-24 18:15:52 +0200

    as

--- testgui/src/test2/test2.java
+++ testgui/src/test2/test2.java
@@ -3,6 +3,14 @@ package test2;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Choice;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Panel;
+import javax.swing.JCheckBox;
 
 public class test2 {
 
@@ -38,6 +46,23 @@ public class test2 {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JCheckBox chckbxHi = new JCheckBox("hi");
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(60)
+					.addComponent(chckbxHi)
+					.addContainerGap(277, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(60)
+					.addComponent(chckbxHi)
+					.addContainerGap(178, Short.MAX_VALUE))
+		);
+		frame.getContentPane().setLayout(groupLayout);
 	}
-
 }
